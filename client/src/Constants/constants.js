e2402d4e447a3780bba58461873c0defe26f3165
Sania Singla <singlasania7717@gmail.com>
2025-01-10 22:54:27 +0530

    k

--- client/src/Constants/constants.js
+++ client/src/Constants/constants.js
@@ -1,6 +1,7 @@
 const LIMIT = 10;
 const DEFAULT_RTE_TEXT = 'Welcome to Post Manager ⭐';
 import SANIAIMAGE from '../Assets/images/sania.jpg';
+import VASUNDHRAIMAGE from '../Assets/images/vasundhra.jpg';
 import LOGO from '../Assets/images/logo.jpg';
 const EMAIL = 'peerconnect@gmail.com';
 const CONTACTNUMBER = 'xxxxxxxxxx';
@@ -17,6 +18,19 @@ const CONTRIBUTORS = [
             gitHub: 'https://github.com/Sania-Singla',
             threads: 'https://x.com/sania_singla',
             instagram: 'https://www.instagram.com/sania__singla',
+        },
+    },
+    {
+        image: VASUNDHRAIMAGE,
+        role: 'Modularity Handler',
+        bio: 'Breaking big ideas into small, reusable components.',
+        name: 'Vasundhra Gupta',
+        socials: {
+            linkedIn: 'https://www.linkedin.com/in/vasundhra-gupta-764713291',
+            discord: '',
+            gitHub: 'https://github.com/Vasundhra-Gupta',
+            threads: '',
+            instagram: 'https://www.instagram.com/vasundhragupta962',
         },
     },
 ];
