00b8327f66a71a80d65e2aa8750e11cb5d11ab59
Sania Singla <singlasania7717@gmail.com>
2025-05-14 17:51:38 +0530

    done

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,16 +6,32 @@ import path from 'path';
 dotenv.config({ path: './src/Config/.env' }); // no need if in root dir
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react({
+            // for better HMR with Tailwind
+            babel: {
+                plugins: [['babel-plugin-macros']],
+            },
+        }),
+    ],
+    server: {
+        watch: {
+            // Ignore all files except Tailwind-related ones
+            ignored: [
+                '**/node_modules/**',
+                '**/.git/**',
+                '!**/src/**/*.css',
+                '!**/tailwind.config.js',
+            ],
+        },
+        // proxy doesn't work in deployment
+        // proxy: {
+        //     '/api': 'http://localhost:3000',
+        // },
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src'),
         },
     },
-    // proxy doesn't work in deployment
-    // server: {
-    //     proxy: {
-    //         '/api': 'http://localhost:3000',
-    //     },
-    // },
 });
