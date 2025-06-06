e209777c4dd76de35f8f69a5373930187f0edcd9
Sania Singla <singlasania7717@gmail.com>
2025-06-07 02:35:12 +0530

    github

--- server/src/schemas/resume.Schema.js
+++ server/src/schemas/resume.Schema.js
@@ -47,6 +47,7 @@ const projectSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     technologies: { type: [String], required: true },
+    demo: { type: String, required: false },
     github: { type: String, required: false },
 });
 
